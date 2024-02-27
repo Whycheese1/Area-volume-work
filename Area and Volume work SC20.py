5a4de2691e9c827671ea781c612d35eeb6032949
Whycheese1 <160049443+Whycheese1@users.noreply.github.com>
2024-02-27 10:16:56 -0600

    Add files via upload

--- Area and Volume work SC20.py
+++ Area and Volume work SC20.py
@@ -1,10 +1,10 @@
 XS = int( input("what is side cube side length?") )
-side = XS #singular side of XS
-Area = XS * XS  #The Area of XS, XS timing with XS
-volume = side*side*side #The Volume of XS, the side of XS
+Side = XS ##  singular side of XS
+Area = str(XS*XS*6) ##  XS * XS*6  ##  The Area of XS, XS timing with XS
+Volume = str(XS*XS*XS*2)  ##  side * side * side ## The Volume of XS, XS times with itself 3 times
 
-XA = int( input("what is the Area of XS?") )
 
-print("the side length of XS is:", XS," and the Area of cube is: ", XA)
 
-print("what is the volume of XS?: ")
+print("the answer is:", XS, "the answer is:", Area)
+
+print("the answer is:", Volume)
